69015d25a7212b5017637f5599c43d508cd0388c
AzubuikeKevin <azubuike.okolie@first-ally.com>
2024-01-12 16:26:38 +0100

    Working with variables and object browser

--- QuickLearn/Program.cs
+++ QuickLearn/Program.cs
@@ -1,4 +1,16 @@
 ﻿// See https://aka.ms/new-console-template for more information
 byte number = 0;
-Console.WriteLine(number);
 
+var count = 10;
+var totalPrice = 23.5f;
+var character = 'A';
+var firstName = "Azubuike";
+var isWorking = true;
+
+Console.WriteLine(number);
+Console.WriteLine(count);
+Console.WriteLine(totalPrice);
+Console.WriteLine(character);
+Console.WriteLine(firstName);
+Console.WriteLine(isWorking);
+Console.BackgroundColor = ConsoleColor.Green;

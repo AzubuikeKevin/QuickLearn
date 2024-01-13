102eba1bac82ee76f84ca4023781392614e90cd6
AzubuikeKevin <azubuike.okolie@first-ally.com>
2024-01-13 14:28:59 +0100

    type conversion

---
+++ TypeConversion/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+try
+{
+	var number = "Hello You!";
+	byte b = Convert.ToByte(number);
+    Console.WriteLine(number);
+}
+catch (Exception)
+{
+
+    Console.WriteLine("Unable to convert the number to byte");
+}
+

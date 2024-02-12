1402f62af69a936392e34779983952a113290aa3
AzubuikeKevin <azubuike.okolie@first-ally.com>
2024-02-12 12:20:39 +0100

    Intro to c# classes

---
+++ Classes/Program.cs
@@ -0,0 +1,31 @@
+﻿// See https://aka.ms/new-console-template for more information
+public class Person
+{
+    public string FirstName { get; set; }
+    public string LastName = "Kevin";
+
+    public void Introduce()
+    {
+        Console.WriteLine("Hello my name is " + FirstName);
+    }
+}
+
+public class Calculator
+{
+    public static int Add(int a, int b)
+    {
+        return a + b;
+    }
+}
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var person = new Person();
+        person.FirstName = "Azubuike";
+        person.Introduce();
+
+        int result = Calculator.Add(2, 4);
+        Console.WriteLine(result);
+    }
+}

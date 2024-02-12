9c6ccd1f40a0b44b254c32a4375eea7133c9e4d1
AzubuikeKevin <azubuike.okolie@first-ally.com>
2024-02-12 13:41:59 +0100

    Strings

---
+++ Strings/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Strings
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var firstName = "Azubuike";
+            var lastName = "Kevin";
+
+            var fullName = firstName + " " + lastName;
+            var myfullName = string.Format("My name is {0} {1}", lastName, firstName);
+            Console.WriteLine(fullName);
+            Console.WriteLine(myfullName);
+
+            var numbers = new int[4] { 1, 2, 3,4};
+            string list = string.Join(" | ", numbers);
+            Console.WriteLine("These are the list of numbers \n" + list);
+        }
+    }
+}

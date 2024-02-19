f9c6e57ac7dc692ef3181e5e55092e2e2d0d5d4d
AzubuikeKevin <azubuike.okolie@first-ally.com>
2024-02-19 18:49:16 +0100

    string builder

--- StrBuilder/Program.cs
+++ StrBuilder/Program.cs
@@ -6,12 +6,16 @@ namespace StrBuilder
     {
         static void Main(string[] args)
         {
-            var builder = new StringBuilder();
-            builder.Append('-', 10);
-            builder.AppendLine();
-            builder.Append("Header");
-            builder.AppendLine();
-            builder.Append('-', 10);
+            var builder = new StringBuilder("Hello World");
+            builder.Append('-', 10)
+                    .AppendLine()
+                    .Append("Header")
+                    .AppendLine()
+                    .Append('-', 10)
+
+                    .Replace('-', '+')
+                    .Remove(0, 10)
+                    .Insert(0, new string('-', 10));
             Console.WriteLine(builder);
         }
     }

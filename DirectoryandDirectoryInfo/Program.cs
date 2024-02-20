75943e87db75608504e4e120763a23c63c048fb1
AzubuikeKevin <azubuike.okolie@first-ally.com>
2024-02-20 14:01:43 +0100

    Direcretory and DeirectoryInfo

---
+++ DirectoryandDirectoryInfo/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace DirectoryandDirectoryInfo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Directory.CreateDirectory(@"c:\temp\folder1");
+
+            var files = Directory.GetFiles(@"c:\temp\folder2", "*.*", SearchOption.AllDirectories);
+            foreach (var file in files)
+                Console.WriteLine(file);
+
+            var directories = Directory.GetDirectories(@"c:\dev1\folder", "*.*", SearchOption.AllDirectories);
+            foreach(var directory in directories) 
+                Console.WriteLine(File.ReadAllText(directory));
+
+            Directory.Exists("..");
+
+
+            // Directory Info
+            var directoryInfo = new DirectoryInfo("");
+            directoryInfo.GetFiles();
+            directoryInfo.GetDirectories();
+
+        }
+    }
+}

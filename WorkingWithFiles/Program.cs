c48f1187a587d2f774b7cc5061a3a4bff705d1ba
AzubuikeKevin <azubuike.okolie@first-ally.com>
2024-02-20 13:50:10 +0100

    File and FileInfo

---
+++ WorkingWithFiles/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace WorkingWithFiles
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var path = @"c:\somefile.jpg";
+
+            File.Copy(@"c:\folder\asset.jpg", path, true);
+            File.Delete(path);
+            if (File.Exists(path))
+            {
+                //
+            }
+            var content = File.ReadAllText(path);
+
+            //File Info (would have to create an Object)
+            var fileInfo = new FileInfo(path);
+            fileInfo.CopyTo("");
+            fileInfo.Delete();
+            if (fileInfo.Exists)
+            {
+                //
+            }
+        }
+    }
+}

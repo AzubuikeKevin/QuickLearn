2d4f9a7a02304482bff8ea68e71b9a3615204d27
AzubuikeKevin <azubuike.okolie@first-ally.com>
2024-02-20 13:20:06 +0100

    code cleanup

--- ProcProgramming/Program.cs
+++ ProcProgramming/Program.cs
@@ -16,8 +16,7 @@
             for (var i = name.Length; i > 0; i--)
                 array[name.Length - i] = name[i - 1];
 
-            var reversed = new string(array);
-            return reversed;
+           return new string(array);
         }
     }
 }
